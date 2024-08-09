b9a6a51b402016f31e1f0ab2486295676a6baa79
vitorhxnrique <162032742+vitorhxnrique@users.noreply.github.com>
2024-08-09 15:09:38 -0300

    Add files via upload

---
+++ wagn2.py
@@ -0,0 +1,7 @@
+numero1 = int(input("digite primeiro numero "))
+numero2 = int(input("digite segundo numero "))
+
+if numero1 > numero2:
+    print("o maior numero e " + str(numero1) + ".")
+else:
+    print("o maior numero e " + str(numero2) + ".")
